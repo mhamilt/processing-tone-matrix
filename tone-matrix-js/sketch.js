16c311dc01ab83e4a15b3131aed745955f02acdf
mhamilt <matt.hamilton@ed.ac.uk>
2019-05-14 10:12:55 +0100

    add bootstrap

--- tone-matrix-js/sketch.js
+++ tone-matrix-js/sketch.js
@@ -25,92 +25,96 @@ var drawStyle;        // are we adding or removing blocks
 //------------------------------------------------------------------------------
 function midi2Hz(midiNoteNumber)
 {
-  return Math.pow(2, (midiNoteNumber - 69) / 12.0) * 440.0;
+    return Math.pow(2, (midiNoteNumber - 69) / 12.0) * 440.0;
 }
+
 //------------------------------------------------------------------------------
 function playNote(step, note)
 {
-  osc_bank[step][note].amp(gain, attack);
-  osc_bank[step][note].amp(gain * sustain, decay);
-  osc_bank[step][note].amp(0.0, release);
+    osc_bank[step][note].amp(gain, attack);
+    osc_bank[step][note].amp(gain * sustain, decay);
+    osc_bank[step][note].amp(0.0, release);
 }
 
 function playSound()
 {
-  if (beat == 0)
-  {
-    // do something on every sequence loop
-  }
-
-  for (var i = 0; i < 16; i++)
-  {
-    if (pat.getStep(beat, i))
+    if (beat == 0)
     {
-      playNote(beat, i);
+        // do something on every sequence loop
     }
-  }
+
+    for (var i = 0; i < 16; i++)
+    {
+        if (pat.getStep(beat, i))
+        {
+            playNote(beat, i);
+        }
+    }
 }
+
 //------------------------------------------------------------------------------
 function setup()
 {
-  createCanvas(screensize + spacer, screensize + spacer);
-  background(0);
-
-  for (var j = 0; j < s; j++)
-  {
-    osc_bank.push([]);
-    for (var i = 0; i < s; i++)
+    var canvas = createCanvas(screensize + spacer, screensize + spacer);
+    background(0);
+    canvas.parent('jumbo-canvas');
+    for (var j = 0; j < s; j++)
     {
-      osc_bank[j].push(new p5.Oscillator());
-      osc_bank[j][i].setType('sine');
-      osc_bank[j][i].freq(midi2Hz(note[i]));
-      osc_bank[j][i].amp(0);
-      osc_bank[j][i].start();
+        osc_bank.push([]);
+        for (var i = 0; i < s; i++)
+        {
+            osc_bank[j].push(new p5.Oscillator());
+            osc_bank[j][i].setType('sine');
+            osc_bank[j][i].freq(midi2Hz(note[i]));
+            osc_bank[j][i].amp(0);
+            osc_bank[j][i].start();
+        }
     }
-  }
 }
 
 //------------------------------------------------------------------------------
 
 function draw()
 {
-  pat.draw();
-  if ((frameCount % framesPerBeat) == 0)
-  {
-    playSound();
-    beat++;
-    beat %= note.length;
-  }
+    pat.draw();
+    if ((frameCount % framesPerBeat) == 0)
+    {
+        playSound();
+        beat++;
+        beat %= note.length;
+    }
 }
+
 //------------------------------------------------------------------------------
 function mouseDragged()
 {
-  var note = Math.floor(constrain((mouseX * s) / width, 0, s - 1));
-  var beat = Math.floor(constrain((mouseY * s) / height, 0, s - 1));
+    var note = Math.floor(constrain((mouseX * s) / width, 0, s - 1));
+    var beat = Math.floor(constrain((mouseY * s) / height, 0, s - 1));
 
-  if(!drawLock)
-  {
-    drawStyle = !pat.getStep(note, beat);
-    drawLock = true;
-  }
+    if (!drawLock)
+    {
+        drawStyle = !pat.getStep(note, beat);
+        drawLock = true;
+    }
 
-  pat.setStepNote(note, beat, drawStyle);
+    pat.setStepNote(note, beat, drawStyle);
 }
 
 function mouseReleased()
 {
-  drawLock = false;
+    drawLock = false;
 }
+
 //------------------------------------------------------------------------------
 function keyPressed()
 {
-  switch (key)
-  {
-    case ' ':
-      pat.clear();
-      break;
-    default:
-      return false;
+    switch (key)
+    {
+        case ' ':
+            pat.clear();
+            break;
+        default:
+            return false;
 
-  }
+    }
 }
